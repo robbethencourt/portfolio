49b520f04010c33589413ef04c1b21bb33fa334f
Robert Bethencourt <robert.bethencourt@gmail.com>
2018-04-06 10:50:03 -0400

    blog post titles now showig on blog page

--- app/public/javascript/scripts.js
+++ app/public/javascript/scripts.js
@@ -1,49 +1,66 @@
 /* global  $ */
 // Javascript function that wraps everything
-$(document).ready(function () {
-   // scroll script for nav links
-  $('.back-to-top').on('click', function (e) {
+$(document).ready(function() {
+  // scroll script for nav links
+  $(".back-to-top").on("click", function(e) {
     // prevent default anchor click behavior
-    e.preventDefault()
+    e.preventDefault();
 
     // store hash
-    var hash = this.hash
+    var hash = this.hash;
 
     // animate
-    $('html, body').animate({
-      scrollTop: $(hash).offset().top
-    }, 300, function () {
-      // when done, add hash to url
-      // (default click behaviour)
-      window.location.hash = hash
-    }) // end animate
-  }) // end scroll script
-}) // end jQuery document ready
+    $("html, body").animate(
+      {
+        scrollTop: $(hash).offset().top
+      },
+      300,
+      function() {
+        // when done, add hash to url
+        // (default click behaviour)
+        window.location.hash = hash;
+      }
+    ); // end animate
+  }); // end scroll script
+}); // end jQuery document ready
 
 // function that calculates the percentage of completed items i have on my about page and displays to the screen
-function expandingList () {
+function expandingList() {
   // Elements
   // all the list items
-  var listItems = document.getElementsByClassName('list-item')
+  var listItems = document.getElementsByClassName("list-item");
   // all the list items with the strike-through class
-  var strikeThroughs = document.getElementsByClassName('strike-th')
+  var strikeThroughs = document.getElementsByClassName("strike-th");
   // where to place the final percent on the screen
-  var percentToScreen = document.getElementById('list-percent')
+  var percentToScreen = document.getElementById("list-percent");
 
   // Calculations
   // the percent of items I've yet to learn
-  var thePercent = strikeThroughs.length / listItems.length
+  var thePercent = strikeThroughs.length / listItems.length;
   // the percent as an integer to display
-  thePercent = Math.round(thePercent.toFixed(2) * 100)
+  thePercent = Math.round(thePercent.toFixed(2) * 100);
 
   // Text Node
   // create a text node off the calculated percentage
-  var percentNode = document.createTextNode(thePercent)
+  var percentNode = document.createTextNode(thePercent);
   // if statement so i don't get a js error on pages without the element to append to
   if (percentToScreen) {
     // append the percent calculated to the element on the screen
-    percentToScreen.appendChild(percentNode)
+    percentToScreen.appendChild(percentNode);
   }
 } // end expandingList()
 
-expandingList()
+expandingList();
+
+/* copyright date at bottom of footer */
+function getCurrentYearString() {
+  return `${new Date().getFullYear()}`;
+}
+
+function addYearToHtml() {
+  let jsYear = document.getElementById("js-date");
+  const textNodeToAppend = document.createTextNode(getCurrentYearString());
+  jsYear.appendChild(textNodeToAppend);
+}
+
+addYearToHtml();
--- app/routing/html-routes.js
+++ app/routing/html-routes.js
@@ -1,166 +1,178 @@
-const axios = require('axios')
+const axios = require("axios");
 
-module.exports = function (app) {
+module.exports = function(app) {
   // html get requests
-  app.get('/projects', function (req, res) {
-    res.render('projects', {
-      title: 'Projects',
+  app.get("/projects", function(req, res) {
+    res.render("projects", {
+      title: "Projects",
       active_projects: true
-    })
-  })
+    });
+  });
 
   // route url root to projects page
-  app.get('/', function (req, res) {
-    res.render('projects', {
-      title: 'Projects',
+  app.get("/", function(req, res) {
+    res.render("projects", {
+      title: "Projects",
       active_projects: true
-    })
-  })
+    });
+  });
 
-  app.get('/crisiscorps', function (req, res) {
-    res.render('crisiscorps', {
-      title: 'Projects - Crisiscorps',
+  app.get("/crisiscorps", function(req, res) {
+    res.render("crisiscorps", {
+      title: "Projects - Crisiscorps",
       active_projects: true
-    })
-  })
+    });
+  });
 
-  app.get('/hangman', function (req, res) {
-    res.render('hangman', {
-      title: 'Projects - Hangman',
+  app.get("/hangman", function(req, res) {
+    res.render("hangman", {
+      title: "Projects - Hangman",
       active_projects: true
-    })
-  })
+    });
+  });
 
-  app.get('/trivia', function (req, res) {
-    res.render('trivia', {
-      title: 'Projects - Trivia',
+  app.get("/trivia", function(req, res) {
+    res.render("trivia", {
+      title: "Projects - Trivia",
       active_projects: true
-    })
-  })
+    });
+  });
 
-  app.get('/aniloop', function (req, res) {
-    res.render('aniloop', {
-      title: 'Projects - Aniloop',
+  app.get("/aniloop", function(req, res) {
+    res.render("aniloop", {
+      title: "Projects - Aniloop",
       active_projects: true
-    })
-  })
+    });
+  });
 
-  app.get('/rpg', function (req, res) {
-    res.render('rpg', {
-      title: 'Projects - RPG',
+  app.get("/rpg", function(req, res) {
+    res.render("rpg", {
+      title: "Projects - RPG",
       active_projects: true
-    })
-  })
+    });
+  });
 
-  app.get('/rockpaperscissor', function (req, res) {
-    res.render('rockpaperscissor', {
-      title: 'Projects - Rock Paper Scissor Multiplayer',
+  app.get("/rockpaperscissor", function(req, res) {
+    res.render("rockpaperscissor", {
+      title: "Projects - Rock Paper Scissor Multiplayer",
       active_projects: true
-    })
-  })
+    });
+  });
 
-  app.get('/alpha', function (req, res) {
-    res.render('alpha', {
-      title: 'Projects - Alpha Logistics',
+  app.get("/alpha", function(req, res) {
+    res.render("alpha", {
+      title: "Projects - Alpha Logistics",
       active_projects: true
-    })
-  })
+    });
+  });
 
-  app.get('/pilates-boutique', function (req, res) {
-    res.render('pilates-boutique', {
-      title: 'Projects - Pilates Boutique',
+  app.get("/pilates-boutique", function(req, res) {
+    res.render("pilates-boutique", {
+      title: "Projects - Pilates Boutique",
       active_projects: true
-    })
-  })
+    });
+  });
 
-  app.get('/hmsitip', function (req, res) {
-    res.render('hmsitip', {
-      title: 'Projects - How Much Should I Tip',
+  app.get("/hmsitip", function(req, res) {
+    res.render("hmsitip", {
+      title: "Projects - How Much Should I Tip",
       active_projects: true
-    })
-  })
+    });
+  });
 
-  app.get('/amodamx', function (req, res) {
-    res.render('amodamx', {
-      title: 'Projects - Amoda mx',
+  app.get("/amodamx", function(req, res) {
+    res.render("amodamx", {
+      title: "Projects - Amoda mx",
       active_projects: true
-    })
-  })
+    });
+  });
 
-  app.get('/mandm', function (req, res) {
-    res.render('mandm', {
+  app.get("/mandm", function(req, res) {
+    res.render("mandm", {
       title: "Projects - Meat Deer & McCaughna's Bourbon Supper",
       active_projects: true
-    })
-  })
+    });
+  });
 
-  app.get('/flakas', function (req, res) {
-    res.render('flakas', {
-      title: 'Projects - Flakas Baked Goods',
+  app.get("/flakas", function(req, res) {
+    res.render("flakas", {
+      title: "Projects - Flakas Baked Goods",
       active_projects: true
-    })
-  })
+    });
+  });
 
-  app.get('/mold-specialists', function (req, res) {
-    res.render('mold-specialists', {
-      title: 'Projects - Mold Specialists',
+  app.get("/mold-specialists", function(req, res) {
+    res.render("mold-specialists", {
+      title: "Projects - Mold Specialists",
       active_projects: true
-    })
-  })
+    });
+  });
 
-  app.get('/rixenus', function (req, res) {
-    res.render('rixenus', {
-      title: 'Projects - Rixen US',
+  app.get("/rixenus", function(req, res) {
+    res.render("rixenus", {
+      title: "Projects - Rixen US",
       active_projects: true
-    })
-  })
+    });
+  });
 
-  app.get('/about', function (req, res) {
-    res.render('about', {
-      title: 'About',
+  app.get("/about", function(req, res) {
+    res.render("about", {
+      title: "About",
       active_about: true
-    })
-  })
+    });
+  });
 
-  app.get('/contact', function (req, res) {
-    res.render('contact', {
-      title: 'Contact',
+  app.get("/contact", function(req, res) {
+    res.render("contact", {
+      title: "Contact",
       active_contact: true
-    })
-  })
+    });
+  });
 
-  app.get('/blog', function (req, res) {
-    axios({
-      method: 'GET',
-      url: 'https://api.rss2json.com/v1/api.json',
-      data: {
-        rss_url: 'https://medium.com/feed/@karolismasiulis'
-      }
-    })
+  app.get("/blog", function(req, res) {
+    axios
+      .get(
+        "https://api.rss2json.com/v1/api.json?rss_url=https://medium.com/feed/@rob_bethencourt",
+        { rss_url: "https://medium.com/feed/@rob_bethencourt" }
+      )
       .then(response => {
-        res.render('blog', {
-          title: 'Blog',
+        const itemsFromFeed = response.data.items;
+        // the comments don't seem to have any categories in the json, so that's a way to filter them out from showing on my blog page
+        const blogPosts = itemsFromFeed.filter(
+          item => item.categories.length !== 0
+        );
+        const postsWithReadableDate = blogPosts.map(post => {
+          const dateString = post.pubDate;
+          let jsDate = new Date(Date.parse(dateString));
+          const readableDate = jsDate.toDateString();
+          return {
+            ...post,
+            pubDate: readableDate
+          };
+        });
+        res.render("blog", {
+          title: "Blog",
           active_blog: true,
-          blogs: response.data.items,
+          blogs: postsWithReadableDate,
           hasError: false,
-          error: ''
-        })
+          error: ""
+        });
       })
       .catch(error => {
-        res.render('blog', {
-          title: 'Blog',
+        res.render("blog", {
+          title: "Blog",
           active_blog: true,
           blogs: [],
           hasError: true,
           error: error
-        })
-      })
-  })
+        });
+      });
+  });
 
   // 404 page
-  app.use(function (req, res) {
-    res.render('404', {
-      title: 'Page Not Found'
-    })
-  })
-} // end module.exports()
+  app.use(function(req, res) {
+    res.render("404", {
+      title: "Page Not Found"
+    });
+  });
+}; // end module.exports()
